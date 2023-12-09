052135949571f90f657e7cd4a1f134b9a05f8ddd
EC2 Default User <ec2-user@ip-172-31-15-97.ap-northeast-1.compute.internal>
2023-12-09 12:01:24 +0000

    Add genres-table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_09_115155) do
+ActiveRecord::Schema.define(version: 2023_12_09_120019) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "item_id", null: false
     t.integer "customer_id", null: false
     t.integer "amount", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
